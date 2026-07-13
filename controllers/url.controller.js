32b208c55518a78f411e3b7033a83948bd3ac1fb
Ibrahim Helaly <ibrahimhelaly386@gmail.com>
2026-07-13 14:39:47 +0300

    Prevent duplicate URL aliases

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,24 +1,29 @@
-const Url = require('../models/url.model')
+const Url = require("../models/url.model");
 
-const getShortUrl = async (req , res)=>{
+const getShortUrl = async (req, res) => {
+  const url = await Url.findOne({ alias: req.params.alias });
+  if (!url) {
+    return res.status(404).send("URL Not Found");
+  }
+  res.redirect(url.url);
+};
 
-    const url = await Url.findOne({alias : req.params.alias}) 
-    if(!url){
-       return res.status(404).send("URL Not Found");
-    }
-    res.redirect(url.url)
-}
+const addLongUrl = async (req, res) => {
+  const exist = await Url.findOne({ alias });
+  if (exist) {
+    return res.status(400).json({
+      message: "Alias already exists",
+    });
+  }
+  const url = await Url.create(req.body);
 
-const addLongUrl = async (req , res)=>{
- const url = await Url.create(req.body);
- 
- res.status(200).json({
-     message: "Created Successfully",
-     data:url
- })
-}
+  res.status(200).json({
+    message: "Created Successfully",
+    data: url,
+  });
+};
 
-module.exports ={
-    getShortUrl,
-    addLongUrl
-}
+module.exports = {
+  getShortUrl,
+  addLongUrl,
+};
